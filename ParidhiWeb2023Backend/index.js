8294ae864ce6a4058de17b8744f1136dfcc9cb14
samratpodder <59735939+samratpodder@users.noreply.github.com>
2023-03-26 01:50:00 +0530

    Backend parsing code for mysql to json

--- ParidhiWeb2023Backend/index.js
+++ ParidhiWeb2023Backend/index.js
@@ -14,23 +14,110 @@ const connection = createConnection({
   database: "eventpage",
 });
 
+var sqlRowstoJSON=(rows)=>{
+  var newObj = [];
+  rows.forEach((row) => {
+    var myjson = {};
+    for (const key in row) {
+      if (Object.hasOwnProperty.call(row, key)) {
+        myjson[key] = row[key];
+        
+      }
+    }
+    newObj.push(myjson);
+  });
+  return newObj;
+};
+// var sqltoJSON= async (query,domain)=>{
+
+//   await connection.query(query,(err, result, field) => {
+//       if (err) return console.log(err);
+//       var newObj = {
+//         "domain":`${domain}`,
+//         "domainevents":[]
+//       };
+//       newObj["domainevents"] = sqlRowstoJSON(result);
+//       return newObj;
+//     }
+//   );
+// };
 var getDataQuery = `select * from 
 eventlist left join Domains on eventlist.DomainID = Domains.DomainID 
 union
 select * from
 eventlist right join Domains on eventlist.DomainID = Domains.DomainID`;
 
+const codQuery = "select * from eventlist left join Domains on eventlist.DomainID = Domains.DomainID where eventlist.DomainID = 2  order by eventlist.DomainID";
+const roboQuery = "select * from eventlist left join Domains on eventlist.DomainID = Domains.DomainID where eventlist.DomainID = 1  order by eventlist.DomainID";
+const civQuery = "select * from eventlist left join Domains on eventlist.DomainID = Domains.DomainID where eventlist.DomainID = 6  order by eventlist.DomainID";
+const gamQuery = "select * from eventlist left join Domains on eventlist.DomainID = Domains.DomainID where eventlist.DomainID = 3  order by eventlist.DomainID";
+const eleQuery = "select * from eventlist left join Domains on eventlist.DomainID = Domains.DomainID where eventlist.DomainID = 4 order by eventlist.DomainID";
+const genQuery = "select * from eventlist left join Domains on eventlist.DomainID = Domains.DomainID where eventlist.DomainID = 5  order by eventlist.DomainID";
 app.get("/events", async (req, res) => {
-  await connection.query(
-    "select * from eventlist left join Domains on eventlist.DomainID = Domains.DomainID ",
-    (err, result, field) => {
-      if (err) return console.log(err);
-      res.json(result);
-    }
-  );
+  var eventObjectJson = {
+    "allevents":[]
+  };
+  await connection.query(codQuery,(err, result, field) => {
+    if (err) return console.log(err);
+    var newObj = {
+      "domain":`Coding`
+    };
+    newObj["domainevents"] = sqlRowstoJSON(result);
+    eventObjectJson["allevents"].push(newObj);
+    // res.send(eventObjectJson);
+  }
+);
+await connection.query(eleQuery,(err, result, field) => {
+  if (err) return console.log(err);
+  var newObj = {
+    "domain":`Electrical`
+  };
+  newObj["domainevents"] = sqlRowstoJSON(result);
+  eventObjectJson["allevents"].push(newObj);
+  // res.send(eventObjectJson);
+}
+);
+await connection.query(gamQuery,(err, result, field) => {
+  if (err) return console.log(err);
+  var newObj = {
+    "domain":`Gaming`
+  };
+  newObj["domainevents"] = sqlRowstoJSON(result);
+  eventObjectJson["allevents"].push(newObj);
+  // res.send(eventObjectJson);
+}
+);
+await connection.query(roboQuery,(err, result, field) => {
+  if (err) return console.log(err);
+  var newObj = {
+    "domain":`Robotics`
+  };
+  newObj["domainevents"] = sqlRowstoJSON(result);
+  eventObjectJson["allevents"].push(newObj);
+  // res.send(eventObjectJson);
+}
+);
+await connection.query(genQuery,(err, result, field) => {
+  if (err) return console.log(err);
+  var newObj = {
+    "domain":`General`
+  };
+  newObj["domainevents"] = sqlRowstoJSON(result);
+  eventObjectJson["allevents"].push(newObj);
+  // res.send(eventObjectJson);
+}
+);
+await connection.query(civQuery,(err, result, field) => {
+  if (err) return console.log(err);
+  var newObj = {
+    "domain":`Civil`
+  };
+  newObj["domainevents"] = sqlRowstoJSON(result);
+  eventObjectJson["allevents"].push(newObj);
+  res.send(eventObjectJson);
+}
+);
 });
-//   console.log(results);
-//   res.send(results);
 
 app.listen(port, () => {
   console.log(`listening to port ${port}`);
